78c76919e55d05d1eca5c58ddbcf21a59c2cf135
henrycheung1020 <146144664+henrycheung1020@users.noreply.github.com>
2023-11-06 22:05:29 +0000

    photo create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,7 +16,24 @@ class PhotosController < ApplicationController
     the_id = params.fetch("path_id")
     matching_photos = Photo.where({ :id => the_id})
     the_photo = matching_photos.at(0)
-    
-    render({ :template => "photo_templates/delete"})
+    the_photo.destroy 
+    redirect_to("/photos")
+   
+  end
+
+  def create
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new 
+    a_new_photo.image = input_image 
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save 
+
+    redirect_to("/photos/"+ a_new_photo.id.to_s)
+    #render( :template => "photo_templates/create")
   end
 end
