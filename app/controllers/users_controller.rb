9bc988ffee9e44d6b01b9a7fc5c305ed6d59a5d5
henrycheung1020 <146144664+henrycheung1020@users.noreply.github.com>
2023-11-06 22:47:18 +0000

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def home
+    redirect_to("/users")
+  end 
   def index
     matching_users = User.all
     @list_of_users = matching_users.order({ :username => :asc})
@@ -11,5 +14,17 @@ class UsersController < ApplicationController
     @the_user = matching_usernames.at(0)
 
     render({ :template => "user_template/show"})
+  end
+
+  def create
+    input_username = params.fetch("query_username")
+
+    a_new_user = User.new 
+    a_new_user.username = input_username 
+
+    a_new_user.save
+
+    redirect_to("/photos/"+ a_new_user.id.to_s)
+    #render( :template => "photo_templates/create")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get("/", :controller => "users", :action => "home")
+
   get("/users", { :controller => "users", :action => "index"})
+
+  post("/add_user", { :controller => "users", :action => "create"})
 
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
